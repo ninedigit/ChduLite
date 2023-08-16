302fcfcb3177c4041ce31d44ebe5d6ce2f28f5d6
yurislav <juraj.malach@gmail.com>
2023-08-16 09:59:19 +0200

    reduce linq usage

--- NineDigit.ChduLite/Extensions/ITransportExtensions.cs
+++ NineDigit.ChduLite/Extensions/ITransportExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using NineDigit.SerialTransport;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,13 +6,19 @@ namespace NineDigit.ChduLite
 {
     internal static class ITransportExtensions
     {
+        const int SoftResetBufferSize = 525;
+        const int SoftResetValue = 0x04;
+
         public static async Task ExecuteSoftResetAsync(this ITransport transport, CancellationToken cancellationToken)
         {
             // "soft-reset" je procedura ktora spociva v tom ze:
             // 1. posle sa do CHDU 525 bajtov EOT(0x04), co sposobi ze sa resetne parsovanie packetov
             // 2. vykonat DiscardBuffers, co dropne nezosynchronizovany garbage z bufferov
 
-            var softResetData = Enumerable.Repeat((byte)0x04, 525).ToArray();
+            var softResetData = new byte[SoftResetBufferSize];
+            for (int i = 0; i < SoftResetBufferSize; i++)
+                softResetData[i] = SoftResetValue;
+            
             await transport.WriteAsync(softResetData, cancellationToken).ConfigureAwait(false);
             transport.DiscardBuffers();
         }
